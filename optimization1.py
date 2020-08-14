0ced794e1aa9e3c9fa6e42aa7b30d33bfde0657c
lasnie1 <67284758+lasnie1@users.noreply.github.com>
2020-08-14 15:15:57 -0500

    Modified

--- optimization1.py
+++ optimization1.py
@@ -6,19 +6,22 @@ agent = pywraplp.Solver.CreateSolver('simple_lp_program', 'GLOP')
 #optimization.
 
 def optimization(price, cost, importance, expdemand, credit):
-    quantity = agent.NumVar(0,expdemand,'quantity') #*
-    obfunc = agent.Objective()
-    obfunc.SetCoefficient(quantity,price)
-    obfunc.SetCoefficient(quantity,(-1)*credit*cost)
-    obfunc.SetMaximization()
-    agent.Solve()#*
-    if (importance == "low"):
-        return [obfunc.Value(), (obfunc.Value())/(expdemand)]
-    elif (obfunc.Value() <= (expdemand - 10) and importance == "medium"):
-        return [obfunc.Value() + 10, (obfunc.Value() + 10)/(expdemand)]
-    elif (obfunc.Value() <= (expdemand - 20) and importance == "high"):
-        return [obfunc.Value() + 20, (obfunc.Value() + 20)/(expdemand)]
-    #* These lines are directly influenced by the instructions. The Value property is also based on this
-    #link.
+quantity = agent.NumVar(0,expdemand,'quantity') #*
+obfunc = agent.Objective()
+obfunc.SetCoefficient(quantity,price)
+b = -1
+if credit >= 30:
+    b = -(30/credit)
+obfunc.SetCoefficient(quantity,(b)*credit*cost)
+obfunc.SetMaximization()
+agent.Solve()#*
+if (importance == "Low"):
+    return [obfunc.Value(), (obfunc.Value())/(expdemand)]
+elif (obfunc.Value() <= (expdemand - 10) and importance == "Medium"):
+    return [obfunc.Value() + 10, (obfunc.Value() + 10)/(expdemand)]
+elif (obfunc.Value() <= (expdemand - 20) and importance == "High"):
+    return [obfunc.Value() + 20, (obfunc.Value() + 20)/(expdemand)]
+#* These lines are directly influenced by the instructions. The Value property is also based on this
+#link.
 
-print(optimization(30, 20, "medium", 35, 60))
+print(optimization(30, 20, "Medium", 35, 60))
